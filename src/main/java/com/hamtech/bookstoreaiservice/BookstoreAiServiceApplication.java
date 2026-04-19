9b0e64752a0a28678fc2095344c6097151de6736
TruongIKPK <phitruong20192020@gmail.com>
2026-04-19 22:40:02 +0700

    first commit

---
+++ src/main/java/com/hamtech/bookstoreaiservice/BookstoreAiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstoreaiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreAiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreAiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamtech/bookstoreaiservice/BookstoreAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstoreaiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreAiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
